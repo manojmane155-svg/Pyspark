f18e2d26139fbdff767621eb88a4e8c10c3a02d1
Manoj Mane <manojmane155@gmail.com>
2026-07-29 03:05:00 +0530

    Auto-upload new files at 29-07-2026  3.05.00.66

--- auto_upload.bat
+++ auto_upload.bat
@@ -1,5 +1,5 @@
 cd C:\JupyterDemo\Pyspark
 git add .
-git commit -m "Auto-upload new files"
+git commit -m "Auto-upload new files at %date% %time%"
 git push origin main
 git push origin main > C:\JupyterDemo\gitlog.txt 2>&1
